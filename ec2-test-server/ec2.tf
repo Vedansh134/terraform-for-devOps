16cb42ed3b032ab339f66f78e119ff4b7ab6ddcb
Vedansh kumar <kumarvedansh134@gmail.com>
2026-03-23 15:00:46 +0530

    add ami var, tags

--- ec2-test-server/ec2.tf
+++ ec2-test-server/ec2.tf
@@ -15,9 +15,10 @@ locals {
 }
 
 # Create a ec2 instance
-resource "aws_instance" "testing-server" {
-  ami           = "ami-019715e0d74f695be"
-  instance_type = "t2.micro"
+resource "aws_instance" "testing_server" {
+  #ami           = "ami-0f559c3642608c138"
+  ami           = var.ami
+  instance_type = var.instance_type
 
   # key_name = aws_key_pair.mumbai_key.key_name
   # OR if you're not creating the key in Terraform, use:
@@ -25,16 +26,16 @@ resource "aws_instance" "testing-server" {
 
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.tf-server-sg.id]
-  user_data = local.user_data_script
+  user_data                   = local.user_data_script
 
-  tags = {
-    Name = "testing-server-tf-apsouth" # This is the EC2 instance name in AWS
-    app  = "test-server"
-  }
+  tags = merge(var.additional_tags, {
+      Name = "testing-server-tf-apsouth" # This is the EC2 instance name in AWS
+  })
 
-  root_block_device {
-    volume_size = 30
-    volume_type = "gp3"
+root_block_device {
+    volume_size = var.ec2_root_block_config.var_size
+    volume_type = var.ec2_root_block_config.var_type
+    delete_on_termination = true
     tags = {
       Name = "test-server-root-volume"
     }
